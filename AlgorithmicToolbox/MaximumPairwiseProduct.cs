dd8ef2ee297f53b6328484fce2a4e1276fa44e5b
ngotantinh96 <ngotantinh96@gmail.com>
2023-08-06 22:18:49 +0700

    Update solution name and add MaximumPairwiseProduct

---
+++ AlgorithmicToolbox/MaximumPairwiseProduct.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumPairwiseProduct
+{
+    public class MaximumPairwiseProduct
+    {
+        public static void ExecuteTesting()
+        {
+            var input = Console.ReadLine();
+            var arrayInput = Console.ReadLine();
+            var arrayNumbers = arrayInput.Split(" ").Select(x => long.Parse(x)).ToArray();
+            Console.WriteLine(MaximumPairwiseProduct.MaximumPairwiseProductUsingGreedy(arrayNumbers));
+        }
+        public static long MaximumPairwiseProductUsingSorting(long[] arrayNumbers)
+        {
+            Array.Sort(arrayNumbers);
+            return arrayNumbers[arrayNumbers.Length - 1] * arrayNumbers[arrayNumbers.Length - 2];
+        }
+
+        public static long MaximumPairwiseProductUsingGreedy(long[] arrayNumbers)
+        {
+            long highestNumber = 0;
+            long secondHighestNumber = 0;
+
+            for (int i = 0; i < arrayNumbers.Length; i++)
+            {
+                if (arrayNumbers[i] > highestNumber)
+                {
+                    secondHighestNumber = highestNumber;
+                    highestNumber = arrayNumbers[i];
+                }
+                else if (arrayNumbers[i] > secondHighestNumber)
+                {
+                    secondHighestNumber = arrayNumbers[i];
+                }
+            }
+
+            return highestNumber * secondHighestNumber;
+        }
+    }
+}
---
+++ AlgorithmicToolbox/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace MaximumPairwiseProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaximumPairwiseProduct.ExecuteTesting();
+        }
+    }
+}
